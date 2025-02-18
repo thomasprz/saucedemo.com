e04a9ad7c2785c66a6a949a0b4b6e7a82a4612c3
Thomas <przegralek.thomas@gmail.com>
2025-02-18 23:06:50 +0100

    Ajout des fixtures de pages dans pages.fixture.ts

--- fixtures/pages.fixture.ts
+++ fixtures/pages.fixture.ts
@@ -2,11 +2,15 @@ import { BasePage } from "../pages/base.page";
 import { HomePage } from "../pages/home.page";
 import {test as pagesTest} from '@playwright/test'
 import { InventoryPage } from "../pages/inventory.page";
+import { InventoryItemPage } from "../pages/inventory-item.page";
+import { CartPage } from "../pages/cart.page";
 
 interface Pages{
     base : BasePage
     home : HomePage
     inventory : InventoryPage
+    inventoryitem : InventoryItemPage
+    cart : CartPage
 }
 
 export const pages = pagesTest.extend<Pages>({
@@ -18,6 +22,11 @@ export const pages = pagesTest.extend<Pages>({
     },
     inventory: async ({page}, use) => {
         await use(new InventoryPage(page))
-    }
-
+    },
+    inventoryitem : async ({page}, use) => {
+        await use(new InventoryItemPage(page))
+    },
+    cart : async ({page}, use) => {
+        await use (new CartPage(page))
+    },
 })
