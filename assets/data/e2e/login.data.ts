d11677361c0b63089dfa835fe7b0f05d2f95d9ff
Thomas <przegralek.thomas@gmail.com>
2025-02-19 20:56:09 +0100

    Ajout de login.data.ts pour la gestion des données de connexion

---
+++ assets/data/e2e/login.data.ts
@@ -0,0 +1,9 @@
+export const loginData = {
+        inventoryURL: 'inventory.html',
+      
+        requiredUsername: 'Epic sadface: Username is required',
+        requiredPassword: 'Epic sadface: Password is required',
+        incorrectCredentials: 'Epic sadface: Username and password do not match any user in this service',
+      
+        locked_user: 'Epic sadface: Sorry, this user has been locked out.',
+};
