062a2bc45a6e94803ad9f10441dc1abfba033365
Thomas <przegralek.thomas@gmail.com>
2025-02-20 21:17:03 +0100

    Footer: Ajout des sélecteurs pour les liens du pied de page

--- components/footer.component.ts
+++ components/footer.component.ts
@@ -1,14 +1,14 @@
 import {Page, Locator, expect} from '@playwright/test'
+import { BasePage } from '../pages/base.page'
 
-export class FooterComponent {
-    readonly page : Page
+export class FooterComponent extends BasePage {
     readonly linkTwitter : Locator
     readonly linkFacebook : Locator
     readonly linkLinkedin : Locator
     readonly footerCopyright : Locator
 
     constructor(page:Page){
-        this.page = page
+        super(page)
         this.linkTwitter = page.getByRole('link', {name:"Twitter"})
         this.linkFacebook = page.getByRole('link', {name:"Facebook"})
         this.linkLinkedin = page.getByRole('link', {name:"LinkedIn"})
