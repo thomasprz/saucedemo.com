cc4888703311decd1c1cf2b7e52bbc283df9e01a
Thomas <przegralek.thomas@gmail.com>
2025-02-20 21:17:11 +0100

    Fixtures: Mise à jour des composants partagés pour les tests

--- fixtures/components.fixture.ts
+++ fixtures/components.fixture.ts
@@ -1,13 +1,18 @@
 import { FooterComponent } from "../components/footer.component";
 import {test as componentsTest } from '@playwright/test'
+import { SidebarComponent } from "../components/sidebar.component";
 
 interface Components {
     footer : FooterComponent
+    sidebar : SidebarComponent
 }
 
 export const components = componentsTest.extend<Components>({
     footer : async ({page}, use) => {
         await use(new FooterComponent(page))
+    },
+    sidebar: async({page}, use) => {
+        await use(new SidebarComponent(page))
     },
 
 })
