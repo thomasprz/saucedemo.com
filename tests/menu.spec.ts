f2426caa28bd6d93ed8d2757d9fb451a8c5847f4
Thomas <przegralek.thomas@gmail.com>
2025-02-22 22:19:03 +0100

    Mise à jour des tests liés au parcours menu

--- tests/menu.spec.ts
+++ tests/menu.spec.ts
@@ -1,25 +1,44 @@
 import { test, expect } from "../fixtures/base.fixture";
+import { logger } from "../helpers/logger.helper";
+// Allure Data & Tags
+import * as report from '../assets/data/report/allure.data.json' // * -> Pratique courante avec JSON permettant d'importer toutes les propriétés du fichier JSON sous un seul alias. 
+import * as allure from 'allure-js-commons'; //Importation du module allure
+import { Severity } from 'allure-js-commons'; // Criticité du test
+import { Configuration } from "../config/configuration";
+
 
 test.describe('Test Cases inventory Item saucedemo.com', async () => {
 
-    test.beforeEach(async ({home}) => {
+    test.beforeEach(async ({login}) => {
         // Arrange
         const standardUser = { username: process.env.USERNAME_STANDARD, password: process.env.PASSWORD };
 
         // Act
-        await home.goto();
-        await home.expectHomePage();
-        await home.fillLoginForm(standardUser)
+        await login.goto();
+        await login.expectHomePage();
+        await login.fillLoginForm(Configuration.user, Configuration.password)
+        logger.info(`Running ${test.info().title}`);
+
+        await allure.parentSuite(report.parent_suite.v001); // Organise les tests dans une hiérarchie de suites. Ex : dossier v001
+        await allure.epic(report.epic.application);
+        await allure.feature(report.feature.menu);
+        await allure.severity(Severity.CRITICAL);
+        await allure.owner(report.owner.tpr);
     });
 
-    test('Test Case 1 : Open and Close Menu', async ({sidebar}) => {
+    test.afterEach('Close the page', async ({ base }, testInfo) => {
+        logger.info(`Finished ${testInfo.title} with status ${testInfo.status}`);
+        await base.closePage();
+     })
+
+    test('Open and Close Menu', async ({sidebar}) => {
         await sidebar.openMenu()
         await expect(sidebar.closeMenuButton).toBeVisible();
         await sidebar.closeMenu()
         await expect(sidebar.burgerButton).toBeHidden()
     })
 
-    test('Test Case 2 : Reset App State', async ({inventory, sidebar}) => {
+    test('Reset App State', async ({inventory, sidebar}) => {
         //Arrange
         const product = {
             name: 'Test.allTheThings() T-Shirt (Red)'
@@ -32,24 +51,24 @@ test.describe('Test Cases inventory Item saucedemo.com', async () => {
         await expect(inventory.cartCounter).not.toBeVisible()
     })
 
-    test('Test Case 3 : Visit About', async ({sidebar}) => {
+    test('Visit About', async ({sidebar}) => {
         //Act
         await sidebar.openMenu()
         await sidebar.openAbout()
         await sidebar.expectSauceLabs()
     })
 
-    test('Test Case 4 : Visit All Items', async ({sidebar, inventory}) => {
+    test('Visit All Items', async ({sidebar, inventory}) => {
         //Act
         await sidebar.openMenu()
         await sidebar.openAllItems()
         await inventory.expectInventoryPage()
     })
 
-    test('Test Case 5 : Logout', async ({sidebar, home}) => {
+    test('Logout', async ({sidebar, login}) => {
         //Act
         await sidebar.openMenu()
         await sidebar.openLogout()
-        await home.expectHomePage()
+        await login.expectHomePage()
     })
 })
