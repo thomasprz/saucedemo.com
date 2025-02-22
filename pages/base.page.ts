4476dad6f6b77aa27fa73d6806bb9174d4ce26b5
Thomas <przegralek.thomas@gmail.com>
2025-02-22 22:16:13 +0100

    Modification de la page de base

--- pages/base.page.ts
+++ pages/base.page.ts
@@ -10,4 +10,21 @@ export class BasePage {
     async goto(url='/'){
         await this.page.goto(url)
     }
+
+    async closePage(){
+        await this.page.close()
+    }
+
+      //* Scrolls
+  async scrollDown(): Promise<void> {
+    await this.page.evaluate(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }
+
+  async scrollUp(): Promise<void> {
+    await this.page.evaluate(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
