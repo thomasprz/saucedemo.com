752dbf58c28ddae8cdde8f6ecd25ff09bfd9edb9
Thomas <przegralek.thomas@gmail.com>
2025-02-22 22:16:47 +0100

    Mise à jour de la configuration Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,32 @@
 import { defineConfig, devices } from '@playwright/test';
-
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import * as os from 'os';
 
 export default defineConfig({
   testDir: './tests',
+  outputDir: './output/test-results',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   
-  reporter: 'html',
-  use: {
+  reporter: [
+    ['line'],
+    ['html', { open: 'on-failure', outputFolder: './output/test-reports' }],
+    ['allure-playwright', {
+      resultsDir: './output/allure-results', // Dossier où les résultats bruts seront stockés
+      outputFolder: './output/allure-reports', // Dossier où le rapport final sera généré
+      environmentInfo: {
+        framework: 'Sauce Demo Playwright',
+        os_platform: os.platform(),
+        os_release: os.release(),
+        os_version: os.version(),
+        os_architecture: os.arch(),
+        node_version: process.version,
+      }
+    }]
+    ],
+    
+    use: {
     baseURL: 'https://www.saucedemo.com/',
     testIdAttribute: 'data-test',
   
