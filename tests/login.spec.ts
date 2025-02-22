ce17911903129aa03b79396958c9b7428f255c25
Thomas <przegralek.thomas@gmail.com>
2025-02-22 22:18:47 +0100

    Mise à jour des tests liés au parcours login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,134 +1,123 @@
+//* Fixtures
 import { test, expect } from "../fixtures/base.fixture";
+//* Fake Data Generation
 import {fakeLoginData} from '../factories/login.factory'
+//* Error message Login pour Assertion
 import { loginData } from "../assets/data/e2e/login.data";
+//* Winston Logs (logger.info)
+import { logger } from "../helpers/logger.helper";
+// Allure Data & Tags
+import * as report from '../assets/data/report/allure.data.json' // * -> Pratique courante avec JSON permettant d'importer toutes les propriétés du fichier JSON sous un seul alias. 
+import * as allure from 'allure-js-commons'; //Importation du module allure
+import { Severity } from 'allure-js-commons'; // Criticité du test
+// Class des users
+import { Configuration } from "../config/configuration";
 
-test.describe('Test Cases login saucedemo.com', () => {
+test.describe('Login', { tag: [report.tags.regression] }, () => {
+    test.use({ storageState: { cookies: [], origins: [] } }); // Cette configuration assure que le test démarre dans un état "propre", sans cookies ni données d'origine préexistantes
 
-    test.beforeEach(async ({ home }) => {
-        await home.goto()
-        await home.expectHomePage()
-    })
+    test.beforeEach('Initial Setup', async ({ login }) => {
+        logger.info(`Running ${test.info().title}`);
 
-    test('Test Case 1 : Login with user "standard_user"', async ({home, inventory}) => {
-        //Arrange
-        const standardUser = {
-            username : process.env.USERNAME_STANDARD,
-            password : process.env.PASSWORD,
-        }
+        await allure.parentSuite(report.parent_suite.v001); // Organise les tests dans une hiérarchie de suites. Ex : dossier v001
+        await allure.epic(report.epic.application);
+        await allure.feature(report.feature.authentication);
+        await allure.owner(report.owner.tpr);
+     });
+     
+     test.afterEach('Close the page', async ({ base }, testInfo) => {
+        logger.info(`Finished ${testInfo.title} with status ${testInfo.status}`);
+        await base.closePage();
+     })
+
+test.describe('Login to specific user', () => {
+    test.beforeEach('Add running test title', async () => {
+        await allure.severity(Severity.CRITICAL);
+    });
+
+    test('Login with user "standard_user"', async ({login, inventory}) => {
         //Act
-        await home.fillLoginForm(standardUser)
+        await login.fillLoginForm(Configuration.user, Configuration.password)
         //Expect
         await inventory.expectInventoryPage()
     })
 
-    test('Test Case 2 : Login with user "visual_user"', async ({home, inventory}) => {
-        //Arrange
-        const visualUser = {
-            username : process.env.USERNAME_VISUAL,
-            password : process.env.PASSWORD,
-        }
+    test('Login with user "visual_user"', async ({login, inventory}) => {
         //Act
-        await home.fillLoginForm(visualUser)
+        await login.fillLoginForm(Configuration.userVisual, Configuration.password)
         //Expect
         await inventory.expectInventoryPage()
     })
 
-    test('Test Case 3 : Login with user "error_user"', async ({home, inventory}) => {
-        //Arrange
-        const errorUser = {
-            username : process.env.USERNAME_ERROR,
-            password : process.env.PASSWORD,
-        }
+    test('Login with user "error_user"', async ({login, inventory}) => {
         //Act
-        await home.fillLoginForm(errorUser)
+        await login.fillLoginForm(Configuration.userError, Configuration.password)
         //Expect
         await inventory.expectInventoryPage()
     })
 
-    test('Test Case 4 : Login with user "problem_user"', async ({home, inventory}) => {
-        //Arrange
-        const problemdUser = {
-            username : process.env.USERNAME_PROBLEM,
-            password : process.env.PASSWORD,
-        }
+    test('Login with user "problem_user"', async ({login, inventory}) => {
         //Act
-        await home.fillLoginForm(problemdUser)
+        await login.fillLoginForm(Configuration.userProblem, Configuration.password)
         //Expect
         await inventory.expectInventoryPage()
     })
 
-    test('Test Case 5 : Login with user "performance_user"', async ({home, inventory}) => {
-        //Arrange
-        const performancedUser = {
-            username : process.env.USERNAME_GLITCH,
-            password : process.env.PASSWORD,
-        }
+    test('Login with user "performance_user"', async ({login, inventory}) => {
         //Act
-        await home.fillLoginForm(performancedUser)
+        await login.fillLoginForm(Configuration.userPerformance, Configuration.password)
         //Expect
         await inventory.expectInventoryPage()
     })
+})
 
-    test('Test Case 6 : login with user Wrong Credentials', async ({home, inventory}) => {
+test.describe('Login with error status', () => {
+    test.beforeEach('Add running test title', async () => {
+        await allure.severity(Severity.NORMAL);
+    });
+
+    test('Login with user Wrong Credentials', async ({login, inventory}) => {
         //Arrange
         const wrongCredentialsUser = fakeLoginData()
         //Act
-        await home.fillLoginForm(wrongCredentialsUser)
+        await login.fillLoginForm('test', 'password')
         //Expect
-        await expect(home.errorLoginMessage).toBeVisible()
-        await expect(home.errorLoginMessage).toContainText(loginData.incorrectCredentials)
+        await expect(login.errorLoginMessage).toBeVisible()
+        await expect(login.errorLoginMessage).toContainText(loginData.incorrectCredentials)
     })
 
-    test('Test case 7 : login with Locked User ', async ({home}) => {
-        //Arrange
-        const lockedUser = {
-            username : process.env.USERNAME_LOCKED,
-            password : process.env.PASSWORD,
-        }
+    test('Login with Locked User ', async ({login}) => {
         //Act
-        await home.fillLoginForm(lockedUser)
+        await login.fillLoginForm(Configuration.userLocked, Configuration.password)
         //Assert
-        await expect(home.errorLoginMessage).toBeVisible
-        await expect(home.errorLoginMessage).toContainText(loginData.locked_user)
+        await expect(login.errorLoginMessage).toBeVisible
+        await expect(login.errorLoginMessage).toContainText(loginData.locked_user)
     })
 
-    test('Test case 8 : Login with Empty User ', async ({home}) => {
-        //Arrange
-        const emptyUsernamedUser = {
-            username : '',
-            password : 'thomas',
-        }
+    test('Login with Empty User ', async ({login}) => {
         //Act
-        await home.fillLoginForm(emptyUsernamedUser)
+        await login.fillPassword(Configuration.password)
+        await login.clickButtonLogin()
         //Assert
-        await expect(home.errorLoginMessage).toBeVisible
-        await expect(home.errorLoginMessage).toContainText(loginData.requiredUsername)
+        await expect(login.errorLoginMessage).toBeVisible
+        await expect(login.errorLoginMessage).toContainText(loginData.requiredUsername)
     })
 
-    test('Test case 9 : Login with Empty Password ', async ({home}) => {
-        //Arrange
-        const emptyPasswordUser = {
-            username : 'thomas',
-            password : '',
-        }
+    test('Login with Empty Password ', async ({login}) => {
         //Act
-        await home.fillLoginForm(emptyPasswordUser)
+        await login.fillUsername(Configuration.user)
+        await login.clickButtonLogin()
         //Assert
-        await expect(home.errorLoginMessage).toBeVisible
-        await expect(home.errorLoginMessage).toContainText(loginData.requiredPassword)
+        await expect(login.errorLoginMessage).toBeVisible
+        await expect(login.errorLoginMessage).toContainText(loginData.requiredPassword)
     })
 
-    test('Test case 10 : Login with Empty Credential ', async ({home}) => {
-        //Arrange
-        const emptyUserData = {
-            username : '',
-            password : '',
-        }
+    test('Login with Empty Credential ', async ({login}) => {
         //Act
-        await home.fillLoginForm(emptyUserData)
+        await login.clickButtonLogin()
         //Assert
-        await expect(home.errorLoginMessage).toBeVisible
-        await expect(home.errorLoginMessage).toContainText(loginData.requiredUsername)
+        await expect(login.errorLoginMessage).toBeVisible
+        await expect(login.errorLoginMessage).toContainText(loginData.requiredUsername)
     })
-
+  })
 })
