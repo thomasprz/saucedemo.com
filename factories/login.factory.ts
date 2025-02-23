fba890af61e73712c304877f923af557b6d39984
Thomas <przegralek.thomas@gmail.com>
2025-02-23 21:16:40 +0100

    Suppression du fichier login.factory.ts

--- factories/login.factory.ts
+++
@@ -1,8 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export function fakeLoginData() {
-    return {
-        username : faker.internet.username(),
-        password : faker.internet.password(),
-    }
-}
