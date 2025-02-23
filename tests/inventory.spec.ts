84cc9c6aa1b9fd67710afe8503f5dd444d862b51
Thomas <przegralek.thomas@gmail.com>
2025-02-23 21:17:29 +0100

    Mise à jour du fichier inventory.spec.ts

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,29 +1,24 @@
 import { test, expect } from "../fixtures/base.fixture"; 
-import data from '../assets/data/e2e/inventory-item.data.json'
+import { inventoryData } from "../assets/data/e2e/inventory.data";
 import { logger } from "../helpers/logger.helper";
 // Allure Data & Tags
 import * as report from '../assets/data/report/allure.data.json' // * -> Pratique courante avec JSON permettant d'importer toutes les propriétés du fichier JSON sous un seul alias. 
 import * as allure from 'allure-js-commons'; //Importation du module allure
 import { Severity } from 'allure-js-commons'; // Criticité du test
-import { Configuration } from "../config/configuration";
-
 
 test.describe('Inventory', {tag : [report.tags.regression]}, () => {
 
-    test.beforeEach(async({login, inventory}) => {
-        const standardUser = {username:process.env.USERNAME_STANDARD, password:process.env.PASSWORD}
-        await login.goto()
-        await login.expectHomePage()
-        await login.fillLoginForm(Configuration.user, Configuration.password)
-        await inventory.expectInventoryPage()
-        logger.info(`Running ${test.info().title}`);
-
-        await allure.parentSuite(report.parent_suite.v001); // Organise les tests dans une hiérarchie de suites. Ex : dossier v001
+    test.beforeEach(async({login, inventory}, testInfo) => {
+        logger.info(`Running ${testInfo.title}`);
+        await allure.parentSuite(report.parent_suite.v001); 
         await allure.epic(report.epic.application);
         await allure.feature(report.feature.inventory);
         await allure.severity(Severity.CRITICAL);
-        await allure.owner(report.owner.tpr);
 
+        // Act
+        logger.info(`Running ${testInfo.title}`);
+        await login.goto(inventoryData.inventoryURL);
+        await inventory.expectInventoryPage();
     })
 
     test.afterEach('Close the page', async ({ base }, testInfo) => {
@@ -31,6 +26,8 @@ test.describe('Inventory', {tag : [report.tags.regression]}, () => {
         await base.closePage();
      })
 
+
+test.describe('Products sorting', () => {
     test('Should List by Name (A to Z)', async ({ inventory }) => {
         const sortOptions = { AZ: "az" };
         await inventory.sortByName(sortOptions.AZ);
@@ -47,6 +44,7 @@ test.describe('Inventory', {tag : [report.tags.regression]}, () => {
         const sortOptions = {HILO : "hilo"}
         await inventory.sortByPrice(sortOptions.HILO)
     })
+})
 
     test('Add to Cart and Then Remove from Cart', async ({inventory}) => {
         //Act
