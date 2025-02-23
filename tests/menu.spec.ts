613a8f3882a658bd8adcff9230f4daf7b2695eae
Thomas <przegralek.thomas@gmail.com>
2025-02-23 21:17:45 +0100

    Mise à jour du fichier menu.spec.ts

--- tests/menu.spec.ts
+++ tests/menu.spec.ts
@@ -5,25 +5,20 @@ import * as report from '../assets/data/report/allure.data.json' // * -> Pratiqu
 import * as allure from 'allure-js-commons'; //Importation du module allure
 import { Severity } from 'allure-js-commons'; // Criticité du test
 import { Configuration } from "../config/configuration";
+import { inventoryData } from "../assets/data/e2e/inventory.data";
 
 
-test.describe('Test Cases inventory Item saucedemo.com', async () => {
+test.describe('Menu', { tag: [report.tags.regression] }, () => {
 
     test.beforeEach(async ({login}) => {
-        // Arrange
-        const standardUser = { username: process.env.USERNAME_STANDARD, password: process.env.PASSWORD };
-
-        // Act
-        await login.goto();
-        await login.expectHomePage();
-        await login.fillLoginForm(Configuration.user, Configuration.password)
         logger.info(`Running ${test.info().title}`);
-
         await allure.parentSuite(report.parent_suite.v001); // Organise les tests dans une hiérarchie de suites. Ex : dossier v001
         await allure.epic(report.epic.application);
         await allure.feature(report.feature.menu);
         await allure.severity(Severity.CRITICAL);
         await allure.owner(report.owner.tpr);
+
+        await login.goto(inventoryData.inventoryURL);
     });
 
     test.afterEach('Close the page', async ({ base }, testInfo) => {
@@ -69,6 +64,5 @@ test.describe('Test Cases inventory Item saucedemo.com', async () => {
         //Act
         await sidebar.openMenu()
         await sidebar.openLogout()
-        await login.expectHomePage()
     })
 })
