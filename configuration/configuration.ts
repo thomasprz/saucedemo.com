f54f8a3c054254d2e8008860e2ffeb86cd367e57
Thomas <przegralek.thomas@gmail.com>
2025-03-17 22:59:08 +0100

    Modification : configuration/configuration.ts

--- configuration/configuration.ts
+++ configuration/configuration.ts
@@ -1,3 +1,27 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config({override:true}) //Permet de charger les variables d'environnement à partir d'un fichier .env
+dotenv.config({ override: true });
+
+export class Configuration {
+  public static get STANDARD_USER(): string {
+    return process.env.STANDARD_USER ?? '[NOT SET]';
+  }
+  public static get LOCKED_OUT_USER(): string {
+    return process.env.LOCKED_OUT_USER ?? '[NOT SET]';
+  }
+  public static get PROBLEM_USER(): string {
+    return process.env.PROBLEM_USER ?? '[NOT SET]';
+  }
+  public static get PERFORMANCE_GLITCH_USER(): string {
+    return process.env.PERFORMANCE_GLITCH_USER ?? '[NOT SET]';
+  }
+  public static get ERROR_USER(): string {
+    return process.env.ERROR_USER ?? '[NOT SET]';
+  }
+  public static get VISUAL_USER(): string {
+    return process.env.VISUAL_USER ?? '[NOT SET]';
+  }
+  public static get PASSWORD(): string {
+    return process.env.PASSWORD ?? '[NOT SET]';
+  }
+}
