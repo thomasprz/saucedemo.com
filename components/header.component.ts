cd660df6e1cb4b075f3023e19f715cb04d2738e9
Thomas <przegralek.thomas@gmail.com>
2025-03-17 22:59:01 +0100

    Modification : components/header.component.ts

--- components/header.component.ts
+++ components/header.component.ts
@@ -4,14 +4,14 @@ import { BasePage } from '../pages/base.page';
 export class HeaderComponent extends BasePage{
     //LOCATOR
     readonly locatorSwagLabsTitle : Locator
-    readonly locatorShoppingCart : Locator
+    readonly locatorShoppingCartBadge : Locator
     readonly locatorButtonMenu : Locator
 
     constructor(page:Page){
         super(page)
         //LOCATOR
         this.locatorSwagLabsTitle = page.getByText('Swag Labs')
-        this.locatorShoppingCart = page.getByTestId('shopping-cart-link')
+        this.locatorShoppingCartBadge = page.getByTestId('shopping-cart-badge')
         this.locatorButtonMenu = page.locator('#bm-burger-button')
     }
 }
