1108b77da5a09c6f91a78f12e7ff44be95746bf8
Thomas <przegralek.thomas@gmail.com>
2025-03-17 23:00:22 +0100

    Modification : tests/auth/auth.setup.ts

--- tests/auth/auth.setup.ts
+++ tests/auth/auth.setup.ts
@@ -1,8 +1,10 @@
 import {test as setup} from "../../fixtures/base.fixture";
+import { Configuration } from "../../configuration/configuration";
 
-setup('authenticate default user', async({login, page}) => {
-  await login.goTo();
-  await login.fillLoginForm()
-  await page.context().storageState({ path: '.auth/user.json' });
-  await page.close();
+setup('Authentification avec le standard_user', async({home, page,inventory}) => {
+    await home.goTo('/')
+    await home.fillLoginForm(Configuration.STANDARD_USER, Configuration.PASSWORD)
+    await home.clickLoginButton()
+    await inventory.expectInventoryPage()
+    await page.context().storageState({ path: '.auth/standard_user.json' });
 })
