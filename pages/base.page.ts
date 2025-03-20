9308b02a717a3053f8ea4316a8387b8bbbbb14ce
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:19:07 +0100

    Amélioration de la mise en page du code

--- pages/base.page.ts
+++ pages/base.page.ts
@@ -15,15 +15,15 @@ export class BasePage {
     async waitFor() {
         await this.page.waitForTimeout(500);
     }
-    
-    async screenshot(screenshotName){
-    await this.page.screenshot({
-      path: `./tests/tests_visual/screenshots/${screenshotName}.png`,
-      fullPage: true,
-    });
-  }
 
-  async expectScreenshot(screenshotName){
-    await expect.soft(this.page).toHaveScreenshot(`${screenshotName}.png`, { fullPage: true });
+    async screenshot(screenshotName){
+        await this.page.screenshot({
+            path: `./tests/tests_visual/screenshots/${screenshotName}.png`,
+            fullPage: true,
+        });
+    }
+
+    async expectScreenshot(screenshotName){
+        await expect.soft(this.page).toHaveScreenshot(`${screenshotName}.png`, { fullPage: true });
   }
 }
