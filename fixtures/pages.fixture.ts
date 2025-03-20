7b53d720c41326f53cebe2693736d399dc75b1af
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:13:42 +0100

    Ajout des pages Cart et Checkout (Step One, Step Two, Complete) dans l'extension pagesTest

--- fixtures/pages.fixture.ts
+++ fixtures/pages.fixture.ts
@@ -1,10 +1,20 @@
 import {test as pagesTest} from '@playwright/test'
 import { HomePage } from "../pages/home.page";
 import { InventoryPage } from '../pages/inventory.page';
+import { CartPage } from '../pages/cart.page';
+import { CheckoutStepOnePage } from '../pages/checkout-step-one.page';
+import { CheckoutStepTwoPage } from '../pages/checkout-step-two.page';
+import { CheckoutCompletePage } from '../pages/checkout-complete.page';
 
 interface Pages{
     home : HomePage
     inventory : InventoryPage
+    cart : CartPage
+    checkout: {
+        one: CheckoutStepOnePage;
+        two: CheckoutStepTwoPage;
+        complete: CheckoutCompletePage;
+    };
 }
 
 export const pages = pagesTest.extend<Pages>({
@@ -14,6 +24,17 @@ export const pages = pagesTest.extend<Pages>({
     },
     inventory: async ({page}, use) => {
         await use(new InventoryPage(page))
+    },
+    cart: async ({page}, use) => {
+        await use(new CartPage(page))
+    },
+    checkout: async ({ page }, use) => {
+        const checkout = {
+            one: new CheckoutStepOnePage(page),
+            two: new CheckoutStepTwoPage(page),
+            complete: new CheckoutCompletePage(page),
+        };
+        await use(checkout);
     },
 })
 
