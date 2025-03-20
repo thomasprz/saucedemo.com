1717cf8daa73bacdea32464847e332d7f4f83b56
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:13:18 +0100

    Ajout du composant Sidebar et extension dans le fichier componentsTest

--- fixtures/components.fixture.ts
+++ fixtures/components.fixture.ts
@@ -1,8 +1,10 @@
 import {test as componentsTest} from '@playwright/test'
 import { HeaderComponent } from '../components/header.component'
+import { SidebarComponent } from '../components/sidebar.component'
 
 interface Components {
     header: HeaderComponent
+    sidebar : SidebarComponent
 }
 
 export const components = componentsTest.extend<Components>({
@@ -10,4 +12,7 @@ export const components = componentsTest.extend<Components>({
     header: async({page}, use) => {
         await use(new HeaderComponent(page))
     },
+    sidebar: async ({page}, use) => {
+        await use(new SidebarComponent(page))
+    }
 })
