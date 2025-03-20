a855c2b29982bcc645943f40231ad100ea50eb1a
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:11:42 +0100

    Ajout des méthodes pour interagir avec le panier et obtenir le nombre d'articles

--- components/header.component.ts
+++ components/header.component.ts
@@ -6,6 +6,7 @@ export class HeaderComponent extends BasePage{
     readonly locatorSwagLabsTitle : Locator
     readonly locatorShoppingCartBadge : Locator
     readonly locatorButtonMenu : Locator
+    readonly locatorCartLink : Locator
 
     constructor(page:Page){
         super(page)
@@ -13,5 +14,16 @@ export class HeaderComponent extends BasePage{
         this.locatorSwagLabsTitle = page.getByText('Swag Labs')
         this.locatorShoppingCartBadge = page.getByTestId('shopping-cart-badge')
         this.locatorButtonMenu = page.locator('#bm-burger-button')
+        this.locatorCartLink = page.getByTestId('shopping-cart-link')
+
+    }
+
+    async clickShoppingCartLink(){
+        await this.locatorCartLink.click()
+    }
+
+    async getCurrentCartCount(){
+        const cartBadge = await this.locatorShoppingCartBadge.innerText()
+        return parseInt(cartBadge); 
     }
 }
