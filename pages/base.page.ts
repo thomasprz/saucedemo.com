bc7c7732bb47a86aa40d5d43a194a14dfa9947be
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:14:05 +0100

    Ajout des méthodes de capture d'écran et d'attente dans la classe BasePage

--- pages/base.page.ts
+++ pages/base.page.ts
@@ -1,13 +1,29 @@
-import {Locator,Page} from '@playwright/test'
+import { Page, expect } from '@playwright/test';
+import path from 'path';
 
 export class BasePage {
-    readonly page : Page
+    readonly page: Page;
 
-    constructor(page:Page){
-        this.page = page
+    constructor(page: Page) {
+        this.page = page;
     }
 
     async goTo(url){
-        await this.page.goto(url)
+        await this.page.goto(url);
     }
+
+    async waitFor() {
+        await this.page.waitForTimeout(500);
+    }
+    
+    async screenshot(screenshotName){
+    await this.page.screenshot({
+      path: `./tests/tests_visual/screenshots/${screenshotName}.png`,
+      fullPage: true,
+    });
+  }
+
+  async expectScreenshot(screenshotName){
+    await expect.soft(this.page).toHaveScreenshot(`${screenshotName}.png`, { fullPage: true });
+  }
 }
