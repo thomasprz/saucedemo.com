e7b7cd3359fc5d4585afa19ebcc0c65d16ca445a
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:14:49 +0100

    Ajout des pages et composants pour la gestion des produits dans la page InventoryPage, ajout de méthodes pour ajouter/retirer des produits du panier

--- pages/inventory.page.ts
+++ pages/inventory.page.ts
@@ -1,5 +1,7 @@
 import {Page,Locator, expect} from '@playwright/test'
 import { BasePage } from './base.page';
+import { InventoryItemPage } from './inventory-item.page';
+import { HeaderComponent } from '../components/header.component';
 
 
 export class InventoryPage extends BasePage {
@@ -10,18 +12,24 @@ export class InventoryPage extends BasePage {
     readonly locatorProductSort : Locator
     readonly locatorAddToCart : Locator
     readonly locatorRemoveCart : Locator
+    readonly locatorCartLink : Locator
+    //PAGE
+    readonly item : InventoryItemPage
+    readonly header : HeaderComponent
 
 
     constructor(page:Page){
         super(page)
         //LOCATOR
         this.locatorProductsTitle = page.getByTestId('title')
-        this.locatorProductItemName = page.getByTestId('.inventory_item_name')
+        this.locatorProductItemName = page.getByTestId('inventory-item-name')
         this.locatorProductName = page.getByTestId('inventory-item').getByTestId('inventory-item-name')
         this.locatorProductSort = page.getByTestId('product-sort-container')
         this.locatorAddToCart = page.getByRole('button', {name: "Add to cart"})
         this.locatorRemoveCart = page.getByRole('button', {name: "Remove"})
-
+        //PAGE
+        this.item = new InventoryItemPage(page)
+        this.header = new HeaderComponent(page)
     }
 
     async expectInventoryPage(){
@@ -49,8 +57,31 @@ export class InventoryPage extends BasePage {
         await this.locatorAddToCart.nth(index).click()
     }
 
-    async removeProduct(index){
-        await this.locatorRemoveCart.nth(index).click()
+    async addFirstProductToCart(){
+        await this.locatorAddToCart.first().click()
+    }
+
+    async addOneProductToCart(product) {
+        const productContainer = this.page.getByTestId('inventory-item').filter({ hasText: product})
+        const addToCartButton = productContainer.getByRole('button', { name: 'Add to cart' });
+        await addToCartButton.click();
+    }
+
+    async addProductsToCart(products: { name: string }[]) {
+        for (const product of products) {
+            const productContainer = await this.page.getByTestId('inventory-item').filter({ hasText: product.name });
+            await productContainer.getByRole('button', { name: 'Add to Cart' }).click();
+        }
+    }
+
+    async removeProduct(product){
+        const productContainer = this.page.getByTestId('inventory-item').filter({ hasText: product})
+        const addToCartButton = productContainer.getByRole('button', { name: 'Remove' });
+        await addToCartButton.click();
+    }
+
+    async clickFirstProductTitle(){
+        await this.locatorProductName.first().click()
     }
 
 }
