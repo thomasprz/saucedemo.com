ccca5cbc4d8ee99cc28e56954199e2621e7364cd
Thomas <przegralek.thomas@gmail.com>
2025-03-20 15:00:19 +0100

    Configuration Playwright avec plusieurs projets et options de débogage

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
+  snapshotPathTemplate: './tests/tests_visual/screenshots/{arg}{ext}',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
@@ -19,17 +20,44 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'setup',
-      use: { ...devices['Desktop Chrome'] },
-      testMatch: '**/*.setup.ts',
+        name: 'setup',
+        use: { ...devices['Desktop Chrome'] },
+        testMatch: '**/*.setup.ts',
     },
-    {
-      name: 'Chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        storageState: '.auth/standard_user.json',
-      },
-      dependencies: ['setup'],
-    },
-  ],
+        {
+            name: 'Standard User',
+            testMatch: '**/tests_e2e/*.spec.ts', 
+            testIgnore : '**/tests_e2e/login.spec.ts', 
+            use: {
+                ...devices['Desktop Chrome'],
+                storageState: '.auth/standard_user.json',
+            },
+            dependencies: ['setup'],
+        },
+        {
+            name: 'Error User',
+            testMatch: '**/tests_e2e/*.spec.ts', 
+            testIgnore : '**/tests_e2e/login.spec.ts',
+            use: {
+                ...devices['Desktop Chrome'],
+                storageState: '.auth/error_user.json',
+            },
+            dependencies: ['setup'],
+        },
+        {
+            name: 'Login Tests',
+            testMatch: '**/tests_e2e/login.spec.ts',
+            use: {
+              ...devices['Desktop Chrome'],
+            },
+        },
+        {
+            name: 'Visual',
+            testMatch : '**/tests_visual/visual-ui.spec.ts',
+            testIgnore : '**/tests_visual/base-visual.spec.ts',
+            use: {
+              ...devices['Desktop Chrome'],
+            },
+        },
+],
 });
